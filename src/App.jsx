0abb3022b68e6fcb6f33858c1d70392e2bf3e2d9
Mohit <mohit.joshi@capanicus.com>
2024-02-13 18:17:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,17 @@ const App = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
 
+  const moveSeekerNormally = () => {
+    const seekerThumb = document.getElementById('seeker-thumb');
+    const audioSeeked = document.getElementById('audio-seeked');
+    const per = (audioRef.current.currentTime / audioRef.current.duration) * 100;
+    seekerThumb.style.left = `${per - 1}%`;
+    audioSeeked.style.width = `${per}%`;
+  };
+
   const requestAnimationFrame = useCallback(() => {
     document.getElementById('audio-start-duration').innerText = formatAudioDuration(audioRef.current.currentTime);
+    moveSeekerNormally();
     requestFramID = window.requestAnimationFrame(requestAnimationFrame);
   }, []);
 
@@ -52,6 +61,8 @@ const App = () => {
         };
         audioRef.current.onended = () => {
           document.getElementById('audio-start-duration').innerText = '00:00';
+          document.getElementById('seeker-thumb').style.left = '0%';
+          document.getElementById('audio-seeked').style.width = '0%';
           cancelAnimationFrame(requestFramID);
           setIsPlaying(false);
         };
@@ -74,7 +85,7 @@ const App = () => {
   }, [audioFile, handleLoadAudioFile]);
 
   return (
-    <div className='flex gap-8 items-start flex-col mx-auto select-none'>
+    <div className='flex gap-8 items-start flex-col mx-auto select-none' id='audio-player-container'>
       <input
         type='file'
         accept='audio/*'
@@ -111,9 +122,9 @@ const App = () => {
             <SeekNext5Icon />
           </span>
         </div>
-        <div className='w-full flex justify-between items-center gap-2 text-slate-500'>
+        <div className='w-full flex justify-between items-center gap-4 text-slate-500'>
           <span id='audio-start-duration'>00:00</span>
-          <input type='range' className='w-full' />
+          <Seeker {...{ audioData, setCurrentTime, audioRef }} />
           <span id='audio-end-duration'>{formatAudioDuration(audioData?.duration) ?? '00:00'}</span>
         </div>
       </div>
@@ -122,3 +133,66 @@ const App = () => {
 };
 
 export default App;
+
+const Seeker = () => {
+  const seekerRef = useRef(null);
+  const thumbRef = useRef(null);
+  const audioSeekedRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      if (!isDragging) return;
+      const seekerRect = seekerRef.current.getBoundingClientRect();
+      const offsetX = event.clientX - seekerRect.left;
+      const width = seekerRect.width;
+      let percentage = (offsetX / width) * 100;
+
+      if (percentage < 0) {
+        percentage = 0;
+      } else if (percentage > 100) {
+        percentage = 100;
+      }
+
+      thumbRef.current.style.left = `${percentage - 1}%`;
+      audioSeekedRef.current.style.width = `${percentage}%`;
+    },
+    [isDragging]
+  );
+
+  const handleMouseDown = () => {
+    setIsDragging(true);
+  };
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  useEffect(() => {
+    const ref = thumbRef.current;
+    ref.addEventListener('mousedown', handleMouseDown);
+    document.addEventListener('mouseup', handleMouseUp);
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      ref?.removeEventListener('mousedown', handleMouseDown);
+      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [handleMouseMove, isDragging]);
+
+  return (
+    // track
+    <div ref={seekerRef} id='seeker-track' className='w-full rounded-md shadow-sm h-2 bg-white relative cursor-pointer z-50'>
+      {/* thumb */}
+      <span
+        ref={thumbRef}
+        id='seeker-thumb'
+        className='w-4 h-4 bg-teal-500 hover:bg-teal-600 inline-block rounded-full absolute top-[-4px] cursor-pointer z-30'
+        style={{
+          left: '0%',
+        }}
+      ></span>
+      {/* seeker */}
+      <div ref={audioSeekedRef} id='audio-seeked' className='w-0 rounded-md h-2 bg-blue-400 relative cursor-pointer z-10'></div>
+    </div>
+  );
+};
